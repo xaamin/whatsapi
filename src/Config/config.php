52c2010d8c9df97ef1ce91fb1ebf5923f81994fa
Benjamín Martínez Mateos <isc.bkm@gmail.com>
2015-11-20 12:15:31 -0600

    Removed unnecessary comments

--- src/Config/config.php
+++ src/Config/config.php
@@ -24,37 +24,23 @@ return [
 
     /**
      * These are fake credentials below. Don't even bother trying to use them.
-     *
-     * Now listen up. The identity field seems to screw everyone up. This is how it works.
-     * Whatsapp needs a unique string, 20 characters long when you register, for it to keep track of the device using the service.
-     * When you use THIS API, the identity token provided gets urldecoded to see if it's 20 characters long.
-     *
-     * If it is NOT 20 characters long, the identity token gets hashed using sha1, then urlencoded (so we can save/use it easily as a string)
-     * and finally converted to lower case. This now gives us a unique (to us) 20 character long string.
-     *
-     * If you provide a string (either already URLencoded or a string that when urlDecoded is 20 characters long) that will be used instead of
-     * any processing by the API. This allows you to use an identity that you might already know or have received using another problem eg WART.
-     *
-     * It's up to you.
      */
     'accounts'    => array(
         'default'   => array(
             'nickname' => 'Itnovado',
             'number'   => '5219512132132',
-            'password' => '==87Vf4plh+lvOAvoURjBoKDKwciw=',
-            'identity' => ''
+            'password' => '==87Vf4plh+lvOAvoURjBoKDKwciw='
         ),
         /*
         'another'    => array(
             'nickname' => '',
             'number'   => '',
-            'password' => '',
-            'identity' => ''
+            'password' => ''
         ),
-        'yetanother' => array('nickname' => '',
+        'yetanother' => array(
+            'nickname' => '',
             'number'   => '',
-            'password' => '',
-            'identity' => ''
+            'password' => ''
         )
         */
     ),

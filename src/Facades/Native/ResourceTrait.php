9d138b89bd178bea8ac006946dd021fb8498511c
Benjamín Martínez Mateos <isc-bk@live.com>
2015-11-24 21:47:25 -0600

    Added documentation

--- src/Facades/Native/ResourceTrait.php
+++ src/Facades/Native/ResourceTrait.php
@@ -27,16 +27,31 @@ trait ResourceTrait
 	 */
 	protected static $config;
 
+    /**
+     * Sets the session manager
+     * 
+     * @param \Xaamin\Whatsapi\Sessions\SessionInterface $session
+     */
     public static function setSessionManager(SessionInterface $session)
     {
         static::$session = $session;
     }
 
+    /**
+     * Sets the event listener
+     * 
+     * @param Xaamin\Whatsapi\Contracts\ListenerInterface $listener
+     */
     public static function setEventListener(ListenerInterface $listener)
     {
     	static::$listener = $listener;
     }
 
+    /**
+     * Sets the config to use. See the example config file in Config/config.php
+     * 
+     * @param array $config
+     */
     public static function setConfig(array $config)
     {
     	static::$config = $config;
